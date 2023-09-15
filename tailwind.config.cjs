f95e6d1f3d52630d7bac4e1866227d251dc12be4
Mario Fernandez-Pacheco <mario.fpalb@gmail.com>
2023-09-15 18:04:39 +0200

    FEATURE (home): Add super mario font and letters, shadow WIP

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+    colors: {
+      "super-mario-red": "#E52521",
+      "super-mario-green": "#43B047",
+      "super-mario-yellow": "#FBD000",
+      "super-mario-blue": "#049CD8",
+    },
+  },
+  plugins: [],
+};
